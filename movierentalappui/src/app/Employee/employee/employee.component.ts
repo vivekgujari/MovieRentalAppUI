9a2d82a4386e27fd9bf3e8ad28ce7875c3b6bde2
vivekgujari <vivekgujari@gmail.com>
2022-01-04 05:19:45 -0700

    added top-nav and employee components

---
+++ movierentalappui/src/app/Employee/employee/employee.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+@Component({
+  selector: 'employee',
+  templateUrl: './employee.component.html',
+  styleUrls: ['./employee.component.css']
+})
+export class EmployeeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
